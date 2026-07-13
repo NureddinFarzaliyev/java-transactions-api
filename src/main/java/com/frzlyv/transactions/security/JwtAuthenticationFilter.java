b948c3cb208d2a4370e8cfb7f0bb91f42d4b41ad
NureddinFarzaliyev <nureddin.fa@gmail.com>
2026-07-13 11:36:53 +0400

    Auth: use UserService to reach DB from JwtAuthenticationFilter

--- src/main/java/com/frzlyv/transactions/security/JwtAuthenticationFilter.java
+++ src/main/java/com/frzlyv/transactions/security/JwtAuthenticationFilter.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -21,9 +23,11 @@ import jakarta.servlet.http.HttpServletResponse;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final JwtService jwtService;
+  private final UserDetailsService userDetailsService;
 
-  public JwtAuthenticationFilter(JwtService jwtService) {
+  public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
     this.jwtService = jwtService;
+    this.userDetailsService = userDetailsService;
   }
 
   @Override
@@ -49,10 +53,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     // Authenticate if we have username && not already authenticated
     if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+      // Fetch user data from db
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
       UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-          username, // User identity
+          userDetails, // User identity
           null, // Credentials (We don't need passwords for stateless JWT validation)
-          Collections.emptyList()); // Authorities/Roles
+          userDetails.getAuthorities()); // Authorities/Roles
 
       // Build details from request metadata
       authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
